4a861ca855a7103e0a8197b2b6c5b813fb024698
DESKTOP-83NSNIV\wlsgh <wlsghd1997@gmail.com>
2024-09-02 15:34:37 +0900

    first commit

---
+++ src/main/java/com/rorent/restapi/RestApiApplication.java
@@ -0,0 +1,14 @@
+package com.rorent.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+}
